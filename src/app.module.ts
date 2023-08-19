6d54896f6075b9531bd0026d9f2f4048c72d0965
dolce <changha@acon3d.com>
2023-08-19 10:34:27 +0900

    prisma set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestModule } from './quest/quest.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [QuestModule, RestaurantModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
