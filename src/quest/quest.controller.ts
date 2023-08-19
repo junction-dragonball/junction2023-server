8c08fcf9afc614fa265c897f94a93783e15c51bb
dolce <changha@acon3d.com>
2023-08-19 13:54:16 +0900

    퀘스트 조회

--- src/quest/quest.controller.ts
+++ src/quest/quest.controller.ts
@@ -2,15 +2,40 @@ import { Body, Controller, Get, Param } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Prisma, Quest, User } from '@prisma/client';
 
+type QuestListing = Pick<
+  Quest,
+  'title' | 'thumbnailUrl' | 'description' | 'difficulty' | 'createdAt'
+>;
+
 @Controller('quest')
 export class QuestController {
   constructor(private prisma: PrismaService) {}
 
   @Get()
-  async getAllQuest(): Promise<Quest[]> {
+  async getAllQuest(): Promise<QuestListing[]> {
+    // TODO: append status from prgress
+
     return this.prisma.quest.findMany({
+      select: {
+        thumbnailUrl: true,
+        title: true,
+        description: true,
+        difficulty: true,
+        createdAt: true,
+      },
       where: {
         available: true,
+      },
+    });
+  }
+
+  @Get(':id')
+  async getQuestById(@Param('id') id: string): Promise<Quest | undefined> {
+    // TODO: append in-progress count
+
+    return this.prisma.quest.findUnique({
+      where: {
+        id: Number(id),
       },
     });
   }
